e774164aacad65096e5047ee8c89f1b3da0d40ae
Manuel Woelker <github@manuel.woelker.org>
2024-10-27 22:48:54 +0100

    feat(ui): basic counter UI

---
+++ examples/sandbox.rs
@@ -0,0 +1,144 @@
+use std::fs::File;
+use std::num::NonZeroU32;
+
+use femtovg::renderer::OpenGl;
+use femtovg::{Canvas, Color, FillRule, Paint, Path, Renderer};
+use glutin::surface::Surface;
+use glutin::{context::PossiblyCurrentContext, display::Display};
+use glutin_winit::DisplayBuilder;
+use raw_window_handle::HasRawWindowHandle;
+use winit::dpi::PhysicalPosition;
+use winit::event::{ElementState, Event, MouseButton, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+use winit::{dpi::PhysicalSize, window::Window};
+
+use glutin::{
+    config::ConfigTemplateBuilder,
+    context::ContextAttributesBuilder,
+    display::GetGlDisplay,
+    prelude::*,
+    surface::{SurfaceAttributesBuilder, WindowSurface},
+};
+
+fn main() {
+    println!("Starting XUI");
+    let event_loop = EventLoop::new();
+    let (context, gl_display, window, surface) = create_window(&event_loop);
+
+    let renderer = unsafe { OpenGl::new_from_function_cstr(|s| gl_display.get_proc_address(s).cast()) }
+        .expect("Cannot create renderer");
+
+    let mut canvas = Canvas::new(renderer).expect("Cannot create canvas");
+    canvas.set_size(1000, 600, window.scale_factor() as f32);
+    File::open("assets/fonts/Roboto-Regular.ttf").unwrap();
+    canvas.add_font("assets/fonts/Roboto-Regular.ttf").unwrap();
+
+    let mut mouse_position = PhysicalPosition::new(0., 0.);
+    let mut counter = 0;
+
+    event_loop.run(move |event, _target, control_flow| match event {
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                mouse_position = position;
+                window.request_redraw();
+            }
+            WindowEvent::MouseInput { state: ElementState::Pressed, button: MouseButton::Left, .. } => {
+                counter+=1;
+                window.request_redraw();
+            }
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        Event::RedrawRequested(_) => {
+            render(&context, &surface, &window, &mut canvas, mouse_position, counter);
+        }
+        _ => {}
+    })
+}
+
+fn create_window(event_loop: &EventLoop<()>) -> (PossiblyCurrentContext, Display, Window, Surface<WindowSurface>) {
+    let window_builder = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(1000., 600.))
+        .with_title("Femtovg");
+
+    let template = ConfigTemplateBuilder::new().with_alpha_size(8);
+
+    let display_builder = DisplayBuilder::new().with_window_builder(Some(window_builder));
+
+    let (window, gl_config) = display_builder
+        .build(event_loop, template, |mut configs| configs.next().unwrap())
+        .unwrap();
+
+    let window = window.unwrap();
+
+    let gl_display = gl_config.display();
+
+    let context_attributes = ContextAttributesBuilder::new().build(Some(window.raw_window_handle()));
+
+    let mut not_current_gl_context =
+        Some(unsafe { gl_display.create_context(&gl_config, &context_attributes).unwrap() });
+
+    let attrs = SurfaceAttributesBuilder::<WindowSurface>::new().build(
+        window.raw_window_handle(),
+        NonZeroU32::new(1000).unwrap(),
+        NonZeroU32::new(600).unwrap(),
+    );
+
+    let surface = unsafe { gl_config.display().create_window_surface(&gl_config, &attrs).unwrap() };
+
+    (
+        not_current_gl_context.take().unwrap().make_current(&surface).unwrap(),
+        gl_display,
+        window,
+        surface,
+    )
+}
+
+fn render<T: Renderer>(
+    context: &PossiblyCurrentContext,
+    surface: &Surface<WindowSurface>,
+    window: &Window,
+    canvas: &mut Canvas<T>,
+    square_position: PhysicalPosition<f64>,
+    counter: i32,
+) {
+    // Make sure the canvas has the right size:
+    let size = window.inner_size();
+    canvas.set_size(size.width, size.height, window.scale_factor() as f32);
+
+    canvas.clear_rect(0, 0, size.width, size.height, Color::white());
+
+    // Make smol red rectangle
+    canvas.clear_rect(
+        square_position.x as u32,
+        square_position.y as u32,
+        30,
+        30,
+        Color::rgbf(1., 0., 0.),
+    );
+//    canvas.clear_rect(140, 140, 40, 40, Color::rgbf(1., 0., 0.));
+    let mut path = Path::new();
+    let corner_radius = 10.0;
+    path.rounded_rect_varying(
+        120.0,
+        120.0,
+        200.0,
+        100.0,
+        corner_radius,
+        corner_radius,
+        corner_radius,
+        corner_radius,
+    );
+    canvas.translate(5.0, 5.0);
+    canvas.fill_path(&path, &Paint::color(Color::hsl(0.5, 0.0, 0.8)));
+    canvas.reset();
+    canvas.fill_path(&path, &Paint::color(Color::hsl(0.5, 0.5, 0.5)));
+    canvas.stroke_path(&path, &Paint::color(Color::hsl(0.5, 0.5, 0.0)));
+    canvas.fill_text(140.0, 140.0, format!("Increment {counter}"), &Paint::color(Color::hsl(0.0, 0.0, 0.0)).with_font_size(18.0).with_anti_alias(true)).unwrap();
+
+    // Tell renderer to execute all drawing commands
+    canvas.flush();
+    // Display what we've just rendered
+    surface.swap_buffers(context).expect("Could not swap buffers");
+}
